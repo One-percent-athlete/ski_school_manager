546f6e4042d85ea92a77c40de4971c9c8914a88f
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-04 22:22:03 +0900

    Create login user url

--- manager/urls.py
+++ manager/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('schedule/', views.schedule, name='schedule'),
+
+    path('login_user/', views.login_user, name="login_user"),
 ]
