4afedbfd1b76e6ba67c1491d843395b3b2e5c0aa
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-05 21:16:56 +0900

    Create logout user url

--- manager/urls.py
+++ manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('schedule/', views.schedule, name='schedule'),
 
     path('login_user/', views.login_user, name="login_user"),
+    path('logout_user/', views.logout_user, name="logout_user"),
 ]
