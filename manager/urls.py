e0baebd678dab961a30692346f99d08d747f300b
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-05 21:27:09 +0900

    Create update profile url

--- manager/urls.py
+++ manager/urls.py
@@ -5,10 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('schedule/', views.schedule, name='schedule'),
 
+    path('register_user/', views.register_user, name="register_user"),
     path('login_user/', views.login_user, name="login_user"),
     path('logout_user/', views.logout_user, name="logout_user"),
-    path('register_user/', views.register_user, name="register_user"),
+    path('delete_user/<int:user_id>/', views.delete_user, name="delete_user"),
 
     path('profile_list/', views.profile_list, name="profile_list"),
-    path('delete_user/<int:user_id>/', views.delete_user, name="delete_user"),
+    path('update_profile/<int:profile_id>/', views.update_profile, name="update_profile"),
 ]
