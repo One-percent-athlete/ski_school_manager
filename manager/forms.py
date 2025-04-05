9ca76a014cff8718d57b985fdea8bb2df49bc3ba
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-05 21:18:44 +0900

    Create forms py

---
+++ manager/forms.py
@@ -0,0 +1,22 @@
+class SignUpForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ('username', 'password1', 'password2')
+
+	def __init__(self, *args, **kwargs):
+		super(SignUpForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control w-full p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
+		self.fields['username'].widget.attrs['placeholder'] = 'ユーザー名'
+		self.fields['username'].label = ''
+		self.fields['username'].help_text = '<ul class="form-text text-muted small"><li>記号、スペースなし</li><li>150文字以下</li></ul>'
+
+		self.fields['password1'].widget.attrs['class'] = 'form-control w-full p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
+		self.fields['password1'].widget.attrs['placeholder'] = 'パスワード'
+		self.fields['password1'].label = ''
+		self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>ユーザー名と一致しない</li><li>8文字以上</li><li>数字のみ不可</li></ul>'
+
+		self.fields['password2'].widget.attrs['class'] = 'form-control w-full p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
+		self.fields['password2'].widget.attrs['placeholder'] = '確認パスワード'
+		self.fields['password2'].label = ''
+		self.fields['password2'].help_text = '<ul class="form-text text-muted small"><li>再度パスワード入力</li></ul>'
