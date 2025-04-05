957b0026a669670783b34f0b8707877447a227d1
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-05 21:22:49 +0900

    Create delete user urls

--- manager/urls.py
+++ manager/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login_user/', views.login_user, name="login_user"),
     path('logout_user/', views.logout_user, name="logout_user"),
     path('register_user/', views.register_user, name="register_user"),
+
+     path('delete_user/<int:user_id>/', views.delete_user, name="delete_user"),
 ]
