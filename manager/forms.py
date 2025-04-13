622874834e36d3f7874493ee193423224d580996
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-13 22:47:36 +0900

    Update lesson form

--- manager/forms.py
+++ manager/forms.py
@@ -31,18 +31,8 @@ class UserProfileForm(forms.ModelForm):
        	('其他', '其他'),
         ('教练', '教练'),
         ('管理', '管理'),]
-	fullname = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control w-1/5 mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder':'お名前スペースなし'}))
-	phone = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control w-1/5 mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder':'携帯電話番号: 07012345678'}))
-	note = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control w-1/5 mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder':'注意事項'}))
-	contract_type = forms.ChoiceField(label="雇用形態", choices=CHOICE, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
-	is_active = forms.BooleanField(label="現役中", required=False)
-
-	class Meta:
-		model = Profile
-		fields = ('fullname', 'phone', 'note', 'contract_type', 'is_active')
-
-class LessonForm(forms.ModelForm):
-	COLORS = (
+	
+	COLORS = [
 		('#808080', '灰色'),
         ('#ff6961', '赤色'),
         ('#ffb480', '橙色'),
@@ -52,7 +42,37 @@ class LessonForm(forms.ModelForm):
         ('#59adf6', '青色'),
         ('#9d94ff', '紫色'),
         ('#c780e8', '桃色'),
-    )
+    ]
+
+	fullname = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control w-1/5 mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder':'お名前スペースなし'}))
+	phone = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control w-1/5 mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder':'携帯電話番号: 07012345678'}))
+	note = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control w-1/5 mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder':'注意事項'}))
+	contract_type = forms.ChoiceField(label="雇用形態", choices=CHOICE, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
+	is_active = forms.BooleanField(label="現役中", required=False)
+	color = forms.ChoiceField(label="カレンダー表示色", choices=COLORS, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
+
+	class Meta:
+		model = Profile
+		fields = ('fullname', 'phone', 'note', 'contract_type', 'is_active')
+
+class LessonForm(forms.ModelForm):
+	
+	PAYMENT_TYPES = [
+        ('現金','現金'),
+        ('刷卡', '刷卡'),
+        ('電子支付', '電子支付'),
+        ('未支付', '未支付'),
+        ]
+	
+	PLACE = [
+        ('比洛夫', '比洛夫'),
+        ('花园', '花园'),
+        ('安努', '安努'),
+        ('莫伊哇', '莫伊哇'),
+        ('留寿都', '留寿都'),
+        ('喜乐乐', '喜乐乐'),
+    ]
+
 	attendees = forms.ModelMultipleChoiceField(label="同行者", queryset=Profile.objects.all(), widget=forms.CheckboxSelectMultiple)
 	name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder': '現場名'}))
 	address = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder': '場所'}))
@@ -61,7 +81,8 @@ class LessonForm(forms.ModelForm):
 	finished = forms.BooleanField(label="完了", required=False)
 	start_date = forms.DateField(label='作業開始日', widget=forms.DateInput(attrs={'type': 'date', 'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'}))
 	end_date = forms.DateField(label='作業終了日', widget=forms.DateInput(attrs={'type': 'date', 'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'}))
-	color = forms.ChoiceField(label="カレンダー表示色", choices=COLORS, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
+	payment_type = forms.ChoiceField(label="支付方式", choices=PAYMENT_TYPES, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
+	place = forms.ChoiceField(label="カレンダー表示色", choices=PLACE, widget=forms.RadioSelect(attrs={'class': 'form-check-input'}))
 
 	class Meta:
 		model = Lesson
