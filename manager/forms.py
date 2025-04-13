62db29e9cb63109cfa635313d92bcce138a2fe37
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-13 22:48:06 +0900

    Update profile form

--- manager/forms.py
+++ manager/forms.py
@@ -12,12 +12,12 @@ class SignUpForm(UserCreationForm):
 		super(SignUpForm, self).__init__(*args, **kwargs)
 
 		self.fields['username'].widget.attrs['class'] = 'form-control w-full p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
-		self.fields['username'].widget.attrs['placeholder'] = 'ユーザー名'
+		self.fields['username'].widget.attrs['placeholder'] = '账号名'
 		self.fields['username'].label = ''
 		self.fields['username'].help_text = '<ul class="form-text text-muted small"><li>記号、スペースなし</li><li>150文字以下</li></ul>'
 
 		self.fields['password1'].widget.attrs['class'] = 'form-control w-full p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300'
-		self.fields['password1'].widget.attrs['placeholder'] = 'パスワード'
+		self.fields['password1'].widget.attrs['placeholder'] = '密码'
 		self.fields['password1'].label = ''
 		self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>ユーザー名と一致しない</li><li>8文字以上</li><li>数字のみ不可</li></ul>'
 
