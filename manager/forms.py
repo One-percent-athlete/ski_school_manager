db4b71ce09c199bf1437a55a1b320ddf44e5581d
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-19 21:39:20 +0900

    Update form

--- manager/forms.py
+++ manager/forms.py
@@ -114,7 +114,7 @@ class LessonForm(forms.ModelForm):
         ('喜乐乐', '喜乐乐'),
     ]
 
-	attendees = forms.ModelMultipleChoiceField(label="", queryset=Profile.objects.all(), widget=forms.CheckboxSelectMultiple)
+	attendees = forms.ModelMultipleChoiceField(label="教练", queryset=Profile.objects.all(), widget=forms.CheckboxSelectMultiple)
 	name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder': '客人名'}))
 	address = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', 'placeholder': '場所'}))
 	job_description = forms.CharField(label="",required=False, widget=forms.TextInput(attrs={'class':'form-control mb-4 p-2 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300', "placeholder": "作業内容"}))
