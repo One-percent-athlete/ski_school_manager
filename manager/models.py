0919d0abd2af615467d0fadb4617354a4ffc3212
One-percent-athlete <one.percent.athlete@gmail.com>
2025-04-25 23:57:30 +0900

    Update model

--- manager/models.py
+++ manager/models.py
@@ -109,6 +109,7 @@ class Profile(models.Model):
     accommodation = models.CharField("宿舍", max_length=20, blank=True)
     years_choices = models.IntegerField("経験年数", max_length=50, choices=SKI, default='1年')
     fare_percentage = models.CharField("提成比率", max_length=50, choices=FARE_PECENTAGE, default='20%')
+    full_attendance = models.CharField("全勤提成", max_length=50, choices=FULL_ATTENDANCE, default='3%')
     note = models.CharField("備考", max_length=500, blank=True)
     is_active = models.BooleanField("現役中", default=True)
     date_created = models.DateTimeField("作成日", auto_now_add=True)
